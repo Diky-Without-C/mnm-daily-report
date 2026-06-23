cc09682fda35e88544d8ed66c6a3a761fb20b289
Diky <mnmrpc.diky@gmail.com>
2026-06-23 10:30:25 +0700

    feat: add total afer containers

--- src/features/report/dataToText/report.text.ts
+++ src/features/report/dataToText/report.text.ts
@@ -1,17 +1,24 @@
+import { ITEM_TYPES } from "@/app/constants";
+import type { Report } from "@/app/supabase/report.dto";
+import { ORDER_CATEGORY } from "@/features/orders/order.constants";
 import type {
   GroupedCategories,
   ParsedReport,
   ParsedSales,
 } from "@/lib/xlsx/xlsx.type";
-import type { Report } from "@/app/supabase/report.dto";
-import { CUSTOM_TITLES, EXTRA_INFO } from "../report.constant";
-import { ORDER_CATEGORY } from "@/features/orders/order.constants";
-import { formatNumber } from "@/utils/formatNumber";
 import { capitalize } from "@/utils/capitalize";
 import { formatDate } from "@/utils/formatDate";
-import { CATEGORY_KEYS, HEADER } from "../report.constant";
+import { formatNumber } from "@/utils/formatNumber";
+import {
+  CATEGORY_KEYS,
+  CUSTOM_TITLES,
+  EXTRA_INFO,
+  HEADER,
+} from "../report.constant";
 import { processedData } from "./report.builder";
 import type { ProcessedGroup } from "./report.type";
+
+const EXTRA_INFO_REGEX = new RegExp(`\\b(${EXTRA_INFO.join("|")})\\b`, "i");
 
 const getTitle = ({ name, code }: ParsedReport) => {
   const formattedCode = code.replace(/ -/g, "");
@@ -24,83 +31,112 @@ const groupItemByType = (items: ParsedReport[]) => {
   const map = new Map<string, ParsedReport[]>();
 
   for (const item of items) {
-    map.set(item.type, [...(map.get(item.type) ?? []), item]);
+    if (!map.has(item.type)) {
+      map.set(item.type, []);
+    }
+
+    map.get(item.type)!.push(item);
   }
 
   return [...map.values()];
 };
 
-const getStockLines = (items: ParsedReport[]) => {
-  const itemGroups = groupItemByType(items);
-
-  const stockLines = itemGroups.map((group) => {
+const buildStockLine = (items: ParsedReport[]) => {
+  return groupItemByType(items).map((group) => {
     const isMultiple = group.length > 1;
-    const EXTRA_INFO_REGEX = new RegExp(`\\b(${EXTRA_INFO.join("|")})\\b`, "i");
 
-    const line = group.map((item, index) => {
-      const extraInfo = item.name.match(EXTRA_INFO_REGEX);
-      const prefix = index === 0 ? item.type : " ".repeat(item.type.length);
-      const suffix = isMultiple ? ` (${extraInfo?.[0] ?? index + 1})` : "";
-      const stock = formatNumber(Math.max(0, Number(item.stock)));
+    return group
+      .map((item, index) => {
+        const extraInfo = item.name.match(EXTRA_INFO_REGEX)?.[0];
+        const prefix = index === 0 ? item.type : " ".repeat(item.type.length);
+        const suffix = isMultiple ? ` (${extraInfo ?? index + 1})` : "";
+        const stock = formatNumber(Math.max(0, Number(item.stock)));
 
-      return `${prefix}${suffix}: ${stock}`;
-    });
+        return `${prefix}${suffix}: ${stock}`;
+      })
+      .join("\n");
+  });
+};
 
-    return line.join("\n");
+const buildContainerLine = (orders: Report[]) => {
+  const lines = orders.map((order) => {
+    const { category, type, amount, code, from, number } = order;
+
+    const categoryLine =
+      category === ORDER_CATEGORY.CONTAINER
+        ? `(${from} ${number})`
+        : `(PO.${number}/${from})`;
+
+    return [categoryLine, code, type, formatNumber(amount)].join(" ");
   });
 
-  return stockLines;
+  return ["CONTAINER", ...lines];
 };
 
-const getContainerLine = (order: Report) => {
-  const { category, type, amount, code, from, number } = order;
+const buildTotalLine = (content: ParsedReport[], orders: Report[]) => {
+  const totalOrder = orders.reduce<Record<string, number>>((acc, curr) => {
+    acc[curr.type] = (acc[curr.type] ?? 0) + curr.amount;
+    return acc;
+  }, {});
 
-  const categoryLine =
-    category === ORDER_CATEGORY.CONTAINER
-      ? `(${from} ${number})`
-      : `(PO.${number}/${from})`;
-  const line = [categoryLine, code, type, formatNumber(amount)].join(" ");
+  const totalContent = content.reduce<Record<string, number>>((acc, curr) => {
+    const type = ITEM_TYPES[curr.type as keyof typeof ITEM_TYPES] ?? curr.type;
 
-  return line;
+    acc[type] = (acc[type] ?? 0) + Math.max(0, Number(curr.stock));
+    return acc;
+  }, {});
+
+  const group = [
+    ...new Set([...Object.keys(totalContent), ...Object.keys(totalOrder)]),
+  ];
+
+  return group.map((type, index) => {
+    const total = (totalContent[type] ?? 0) + (totalOrder[type] ?? 0);
+
+    return `${index === 0 ? "TOTAL: " : " ".repeat(7)}${type} ⇒ ${formatNumber(total)} pcs`;
+  });
 };
 
-const getSalesLine = (sales: ParsedSales[]) => {
+const buildSalesLine = (sales: ParsedSales[]) => {
   const totalSales = sales
     .flatMap((sale) => sale.monthlySale.slice(0, 3))
     .reduce((acc, curr) => acc + curr, 0);
 
-  const packing = sales[0]?.packing?.match(/\d+/) ?? "(?)";
-  const salesLine = `MPO: ${formatNumber(totalSales)} ctn x ${packing} ⇒ ${formatNumber(totalSales * Number(packing[0]))} pcs`;
+  const packing = Number(sales[0]?.packing?.match(/\d+/)?.[0] ?? 0);
 
-  return sales.length ? [salesLine] : [];
+  const salesLine = [
+    `MPO: ${formatNumber(totalSales)} ctn`,
+    `x ${packing || "(?)"}`,
+    `⇒ ${formatNumber(totalSales * packing)} pcs`,
+  ].join(" ");
+
+  return sales.length > 0 ? [salesLine] : [];
 };
 
 const buildReportText = (group: ProcessedGroup) => {
   const title = getTitle(group.content[0]);
-  const sales = group.sales ? getSalesLine(group.sales) : [];
-  const content = getStockLines(group.content);
-  const containersLine =
-    group.orders.length > 0
-      ? ["CONTAINER", ...group.orders.map(getContainerLine)]
-      : [];
+  const sales = group.sales ? buildSalesLine(group.sales) : [];
+  const content = buildStockLine(group.content);
+  const containers =
+    group.orders.length > 0 ? buildContainerLine(group.orders) : [];
+  const total =
+    sales.length > 0 ? buildTotalLine(group.content, group.orders) : [];
 
-  return [title, ...sales, ...content, ...containersLine].join("\n");
+  return [title, ...sales, ...content, ...containers, ...total].join("\n");
 };
 
 export const reportToText = (
   data: GroupedCategories,
   date: Date,
-  order: Report[],
+  orders: Report[],
   sales?: ParsedSales[],
 ) => {
-  const normalized = processedData(data, order, sales);
+  const normalized = processedData(data, orders, sales);
 
   const body = CATEGORY_KEYS.map((key) => {
-    const content = normalized[key]
-      .map((group) => buildReportText(group))
-      .join("\n\n");
+    const groups = normalized[key].map(buildReportText).join("\n\n");
 
-    return `*${capitalize(key.replace(/_/g, " "))}\n\n${content}\n`;
+    return `*${capitalize(key.replace(/_/g, " "))}\n\n${groups}\n`;
   });
 
   return [HEADER, formatDate(date), "-".repeat(HEADER.length), ...body].join(
