c038f3bf34e59aab476df9899281bc148b821ff9
Diky <mnmrpc.diky@gmail.com>
2026-07-29 08:18:49 +0700

    style: new design for sidebar

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -9,11 +9,13 @@ export default function Header() {
   const { status } = useOnlineStore();
 
   return (
-    <header className="flex h-16 w-full items-end gap-2 border-b-2 border-gray-400 bg-slate-100/80 px-6 py-3">
-      <Link to="/" className="flex cursor-pointer items-start gap-2">
-        <h1 className="text-3xl font-bold">Daily Report </h1>
+    <header className="relative flex h-16 w-full items-end gap-1 border-b-2 border-gray-400 bg-slate-100/80 px-6 py-3">
+      <div className="flex items-start gap-1">
+        <Link to="/" className="text-3xl font-bold">
+          Daily Report
+        </Link>
         <Badge variant={status === "online" ? "success" : "error"} dot></Badge>
-      </Link>
+      </div>
       <span className="text-lg font-semibold">({currentDate})</span>
     </header>
   );
--- src/layouts/Sidebar/index.tsx
+++ src/layouts/Sidebar/index.tsx
@@ -52,44 +52,49 @@ export default function Sidebar({ expanded, onToggle }: SidebarProps) {
     <aside
       ref={ref}
       className={cn(
-        `fixed top-0 left-0 z-10 flex h-full flex-col bg-gray-800 p-2 shadow-lg transition-[width] duration-300`,
-        expanded ? "w-64 overflow-x-hidden" : "w-16",
+        "fixed inset-y-0 left-0 z-10 flex flex-col bg-gray-900 px-3 shadow-xl transition-[width] duration-300",
+        expanded ? "w-72" : "w-18",
       )}
     >
-      <div className={cn("flex items-center", expanded ? "w-60" : "w-12")}>
+      <header className="mb-5 flex h-16 items-center border-b-2 border-gray-400 py-3">
         <HamburgerButton open={expanded} onToggle={onToggle} />
-      </div>
-      <ul
-        className={cn(
-          expanded ? "w-60" : "w-12",
-          "mt-8 flex flex-col gap-2 text-white",
-        )}
-      >
-        {menuItems.map((item) => {
-          return (
+        <div className={cn("flex items-start gap-1", !expanded && "hidden")}>
+          <h1 className="truncate text-xl text-white">Daily Report</h1>
+        </div>
+      </header>
+      <nav className="flex-1">
+        <ul className="space-y-1">
+          {menuItems.map((item) => (
             <li key={item.path}>
               <NavLink
                 to={item.path}
                 className={({ isActive }) =>
                   cn(
-                    `group relative flex cursor-pointer items-center gap-2 rounded-md p-2 hover:bg-gray-700`,
-                    isActive && "bg-gray-700",
+                    "group relative flex h-12 items-center rounded-lg px-3 transition-colors",
+                    isActive
+                      ? "bg-gray-800 text-white"
+                      : "text-gray-200 hover:bg-gray-800",
                   )
                 }
               >
-                {item.icon}
+                <div className="flex size-6 shrink-0 items-center justify-center">
+                  {item.icon}
+                </div>
                 {expanded ? (
-                  <span className="ml-2 text-lg">{item.label}</span>
-                ) : (
-                  <span className="absolute left-0 ml-2 hidden translate-x-13 items-center rounded-lg bg-black px-2 py-1.5 text-white drop-shadow-lg group-hover:flex after:absolute after:left-0 after:size-2 after:-translate-x-1/2 after:rotate-45 after:bg-inherit after:content-['']">
+                  <span className="ml-3 truncate text-base font-medium">
                     {item.label}
+                  </span>
+                ) : (
+                  <span className="pointer-events-none absolute left-full ml-4 hidden rounded-lg bg-slate-800 px-3 py-2 text-sm font-medium whitespace-nowrap text-white shadow-xl group-hover:flex">
+                    {item.label}
+                    <span className="absolute top-1/2 left-0 h-2 w-2 -translate-x-1/2 -translate-y-1/2 rotate-45 bg-inherit" />
                   </span>
                 )}
               </NavLink>
             </li>
-          );
-        })}
-      </ul>
+          ))}
+        </ul>
+      </nav>
     </aside>
   );
 }
